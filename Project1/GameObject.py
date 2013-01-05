8e9153c74493ced7e29cb721821d272eb06160e8
boni <boni@BoniPc>
2013-01-05 19:18:57 -0200

    Removido: chamada de métodos desnecessarios e implematação do método set_position()

--- Project1/GameObject.py
+++ Project1/GameObject.py
@@ -22,14 +22,18 @@ class GameObject(sf.Sprite):
         self._image = sf.Image()
         self._image .LoadFromFile( thunder_boat_file_name )
         
-        thunder_boat = sf.Sprite( self._image )
-        sf.Sprite.__init__(self)
-        thunder_boat.SetPosition(400, 300)
-        thunder_boat.SetSubRect(sf.IntRect(192, 0, 288, 96))
-        thunder_boat.SetScale( 1, 1 )
+        #thunder_boat = sf.Sprite( self._image )
+        sf.Sprite.__init__( self._image )
+        #thunder_boat.SetPosition(400, 300)
+        #thunder_boat.SetSubRect(sf.IntRect(192, 0, 288, 96))
+        #thunder_boat.SetScale( 1, 1 )
         
     def draw(self):
         pass
     
-    def set_position(self):
-        pass
+    def set_position(self, position ):
+        """
+        Seta a posição do objeto
+        """
+        self.SetPosition( position )
+    #set_position()
